c2b71e862bccaf43081c44858c854fb8e3962262
Dave Lively <davelively14@gmail.com>
2015-10-20 17:03:57 -0400

    Working through Route Guide on phoenixframework.org

--- web/router.ex
+++ web/router.ex
@@ -15,6 +15,18 @@ defmodule HelloPhoenix.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+
+    # NOTE: UserController and PostController do not exist. Used here only to show example of nesting.
+    resources "users", UserController do
+      resources "posts", PostController
+     end
+  end
+
+  # NOTE: no controllers within this scope exist. Used here only to show examples of scoped routes.
+  scope "/admin" do
+    pipe_through :browser
+
+    resources "/reviews", HelloPhoenix.Admin.ReviewController
   end
 
   # Other scopes may use custom stacks.
